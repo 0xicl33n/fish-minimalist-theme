e976de3e667f6e4238dd7eb0f2cceed39cd9e48f
root <root@localhost.localdomain>
2015-05-14 00:34:48 -0700

    FINALLY

---
+++ minimalist-red/fish_prompt.fish
@@ -0,0 +1,28 @@
+#slightly forked from l theme 
+#based off my bash PS1
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
+
+function fish_prompt
+    set -l cyan (set_color cyan)
+    set -l blue (set_color blue)
+    set -l red (set_color red)
+    set -l uzr $red(whoami)
+    set -l cwd $cyan(prompt_pwd)
+    set -l white (set_color white)
+    set -l green (set_color green)
+    set -l host $blue(cat /etc/hostname)
+    if [ (_git_branch_name) ]
+    	set git_info $green(_git_branch_name)
+    	set git_info ":$git_info"
+    if [ (_is_git_dirty) ]
+      	set -l dirty "*"
+      	set git_info "$git_info$dirty"
+      	end
+     end
+    echo -n -s $white'['$uzr$white'@'$host$white'['$cwd $git_info $white']'$white'$'
+end
