3e1a31a791142734a1b363bc90941bd4f68e2cf7
Bonnie <0xicl33n@gmail.com>
2016-03-08 21:56:48 -0800

    Color change to be more readable.

--- minimalist-red/fish_prompt.fish
+++ minimalist-red/fish_prompt.fish
@@ -1,4 +1,4 @@
-#slightly forked from l theme 
+#slightly forked from l theme
 #based off my bash PS1
 function _git_branch_name
   echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
@@ -8,8 +8,8 @@ function _is_git_dirty
 end
 
 function fish_prompt
-    set -l cyan (set_color cyan)
-    set -l blue (set_color blue)
+    set -l cyan (set_color 33FFFF)
+    set -l blue (set_color 0080FF)
     set -l red (set_color red)
     set -l uzr $red(whoami)
     set -l cwd $cyan(prompt_pwd)
@@ -17,12 +17,12 @@ function fish_prompt
     set -l green (set_color green)
     set -l host $blue(cat /etc/hostname)
     if [ (_git_branch_name) ]
-    	set git_info $green(_git_branch_name)
-    	set git_info ":$git_info"
+        set git_info $green(_git_branch_name)
+        set git_info ":$git_info"
     if [ (_is_git_dirty) ]
-      	set -l dirty "*"
-      	set git_info "$git_info$dirty"
-      	end
+        set -l dirty "*"
+        set git_info "$git_info$dirty"
+        end
      end
-    echo -n -s $white'['$uzr$white'@'$host$white'['$cwd $git_info $white']'$white'$'
+    echo -n -s $white'['$uzr$white'@'$host$white'['$cwd $git_info $white']'$whit                                                                                                 e'$'
 end
